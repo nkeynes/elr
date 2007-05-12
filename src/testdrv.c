4680841a9f2ce1a5b1e7ee6b56afee2b7febb15d
Nathan Keynes <nkeynes@deadcoderemoval.net>
2007-05-12 04:04:58 +0000

    Start adding system tests

--- src/testdrv.c
+++
@@ -1,8 +0,0 @@
-int yyparse_snarf_file( char *filename );
-
-int main(int argc, char *argv[] )
-{
-	if( argc > 1 ) {
-	  printf( "Result: %d\n", yyparse_snarf_file( argv[1] ) );
-	}
-}
---
+++ test/testdrv.c
@@ -0,0 +1,9 @@
+int yyparse_file( char *filename );
+
+int main(int argc, char *argv[] )
+{
+	if( argc > 1 ) {
+	  printf( "Result: %d\n", yyparse_file( argv[1] ) );
+	}
+	return 0;
+}
